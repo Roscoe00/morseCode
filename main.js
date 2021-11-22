9fddaa51cf75999df330923dbdcadeefc5a1496d
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-22 16:38:41 +0000

    basic morse translator finished

--- main.js
+++ main.js
@@ -32,17 +32,32 @@ let wordObj = {
 //       return wordArr
 // }
    
-
+export const reverseTranslate = (morse)=> {
+   return Object.keys(wordObj).find(key => wordObj[key] === morse);
+ }
 
 export const translate = (word) =>{
    let translation = ""
    let wordArr = word.split("")
+   let morseArr= word.split(" ")
+   if (wordArr[0] in wordObj){
       for (let i=0; i<word.length; i++){
          if (wordArr[i] in wordObj){
             console.log(wordArr[i])
-            translation = translation + wordObj[wordArr[i]]
-      }else if (wordArr[i]=" "){
+            translation = translation + wordObj[wordArr[i]] 
+         }else if (wordArr[i]=" "){
          translation = translation + " "
+         }
       }
-   }return translation
+         }else if (reverseTranslate(morseArr[0]) in wordObj){
+         for (let i =0; i<word.split(" ").length+1;i++){
+            if (reverseTranslate(morseArr[i]) in wordObj){
+            translation = translation + reverseTranslate(morseArr[i])
+            }else if (morseArr[i]==="/"){
+            translation = translation + " "
+            }
+         }
+      }
+      return translation
 }
+
--- main.test.js
+++ main.test.js
@@ -1,6 +1,6 @@
 // need to test for each letter
 import {expect, it} from '@jest/globals';
-import {createArr, translate, wordArr} from "./main" 
+import {translate, reverseTranslate} from "./main" 
 
 it("should translate a to .-",()=>{
    const result = translate("a");
@@ -19,14 +19,22 @@ it("should translate a b to .- -...",()=>{
    expect(result).toBe(".- -...")
 })
 
-it("should take a string and place each letter into an array",()=>{
-   const result = createArr("hello");
-   expect(result).toStrictEqual(["h","e","l","l","o"])
-})
+// it("should take a string and place each letter into an array",()=>{
+//    const result = createArr("hello");
+//    expect(result).toStrictEqual(["h","e","l","l","o"])
+// })
 
 it("should take a string, place each letter into an array, and then translate each letter",()=>{
    const result = translate("sos");
    expect(result).toBe("...---...");
+})
+it("should take morse and return a string",()=>{
+   const result = translate(".-");
+   expect(result).toBe("a");
+})
+it("should take a morse word and return a string, including spaces between words",()=>{
+   const result = translate(".... .. / - .... . .-. .");
+   expect(result).toBe("hi there");
 })
 
 
